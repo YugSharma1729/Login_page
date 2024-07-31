b2127a571b574a4bb5447772e130a236204bfd7b
YugSharma1729 <yugsharma1107@gmail.com>
2024-07-31 10:05:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+
+function login() {
+    x.style.left = "4px";
+    y.style.right = "-520px";
+    x.style.opacity = 1;
+    y.style.opacity = 0;
+}
+
+function register() {
+    x.style.left = "-510px";
+    y.style.right = "5px";
+    x.style.opacity = 0;
+    y.style.opacity = 1;
+}
+
